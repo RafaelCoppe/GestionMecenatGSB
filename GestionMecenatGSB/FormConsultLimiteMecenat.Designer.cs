6000b0baed8f3ace6e25ff6c8a08ceb34f3a3b21
Dennel <fabien.dennel.jr@gmail.com>
2021-10-19 11:02:54 +0200

    Modif diverse

--- GestionMecenatGSB/FormConsultLimiteMecenat.Designer.cs
+++ GestionMecenatGSB/FormConsultLimiteMecenat.Designer.cs
@@ -29,12 +29,46 @@ namespace GestionMecenatGSB
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dtgConsultLimiteMecenat = new System.Windows.Forms.DataGridView();
+            this.btnObtenirInfos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsultLimiteMecenat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgConsultLimiteMecenat
+            // 
+            this.dtgConsultLimiteMecenat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgConsultLimiteMecenat.Location = new System.Drawing.Point(127, 30);
+            this.dtgConsultLimiteMecenat.Name = "dtgConsultLimiteMecenat";
+            this.dtgConsultLimiteMecenat.Size = new System.Drawing.Size(562, 338);
+            this.dtgConsultLimiteMecenat.TabIndex = 0;
+            // 
+            // btnObtenirInfos
+            // 
+            this.btnObtenirInfos.Location = new System.Drawing.Point(313, 390);
+            this.btnObtenirInfos.Name = "btnObtenirInfos";
+            this.btnObtenirInfos.Size = new System.Drawing.Size(194, 34);
+            this.btnObtenirInfos.TabIndex = 1;
+            this.btnObtenirInfos.Text = "Obtenir les informations";
+            this.btnObtenirInfos.UseVisualStyleBackColor = true;
+            this.btnObtenirInfos.Click += new System.EventHandler(this.btnObtenirInfos_Click);
+            // 
+            // FormConsultLimiteMecenat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnObtenirInfos);
+            this.Controls.Add(this.dtgConsultLimiteMecenat);
+            this.Name = "FormConsultLimiteMecenat";
             this.Text = "FormConsultLimiteMecenat";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsultLimiteMecenat)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dtgConsultLimiteMecenat;
+        private System.Windows.Forms.Button btnObtenirInfos;
     }
 }
--- GestionMecenatGSB/FormConsultLimiteMecenat.cs
+++ GestionMecenatGSB/FormConsultLimiteMecenat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestionMecenatBLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,13 @@ namespace GestionMecenatGSB
         public FormConsultLimiteMecenat()
         {
             InitializeComponent();
+        }
+
+        private void btnObtenirInfos_Click(object sender, EventArgs e)
+        {
+            dtgConsultLimiteMecenat = AvoirPourLimiteDeMecenatManager.GetInstance().GetLimiteDeMecenat();
+
+
         }
     }
 }
