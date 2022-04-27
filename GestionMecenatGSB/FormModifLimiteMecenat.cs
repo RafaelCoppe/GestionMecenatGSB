30835c7510b7718c656aa1bf3565742d3590fee7
Dennel <fabien.dennel.jr@gmail.com>
2022-04-27 15:54:01 +0200

    Update limite de mecenat v5

--- GestionMecenatGSB/FormModifLimiteMecenat.cs
+++ GestionMecenatGSB/FormModifLimiteMecenat.cs
@@ -1,5 +1,4 @@
-﻿
-using GestionMecenatBLL;
+﻿using GestionMecenatBLL;
 using GestionMecenatBO;
 using System;
 using System.Collections.Generic;
@@ -44,6 +43,84 @@ namespace GestionMecenatGSB
             cbxAnnee.ValueMember = "numAnnee";
             cbxAnnee.DataSource = AnneeManager.GetInstance().GetLesAnneesDisponiblePourUnPays((int)cbxPays.SelectedValue); //Récupération des Annees
             cbxAnnee.SelectedItem = null;
+        }
+
+
+        private void dtgConsultLimiteMecenat_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtSommeMax.Text = dtgConsultLimiteMecenat.Rows[e.RowIndex].Cells[0].Value.ToString();
+            limiteMecenatChoisi = true;
+
+            idPays = (int)dtgConsultLimiteMecenat.Rows[e.RowIndex].Cells[5].Value;
+            idAnnee = (int)dtgConsultLimiteMecenat.Rows[e.RowIndex].Cells[3].Value;
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            if (limiteMecenatChoisi == false)
+            {
+                MessageBox.Show("Choisissez une limite de mecenat en double-cliquant sur une cellule", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string ErreursEntrees = ""; //Liste des erreurs
+                if (txtSommeMax.Text.Trim() == "," || txtSommeMax.Text.Trim() == "0,") //Si il n'y a pas de somme
+                {
+                    ErreursEntrees += "Veuillez entrer une somme maximale !\n";
+                    txtSommeMax.Focus(); //Curseur sur le champ qui pose problème
+                }
+
+                if (cbxAnnee.SelectedItem == null) //Si il n'y a pas d'annee
+                {
+                    ErreursEntrees += "Veuillez sélectionner une annee dans la liste déroulante !\n";
+                    if (txtSommeMax.Focused == false) cbxAnnee.Focus();//Si aucun champ n'est focus, on met le curseur sur celui-ci
+                }
+
+                if (cbxPays.SelectedItem == null) //Si il n'y a pas de pays
+                {
+                    ErreursEntrees += "Veuillez sélectionner un pays dans la liste déroulante !";
+                    if (txtSommeMax.Focused == false && cbxAnnee.Focused == false) cbxPays.Focus();//Si aucun champ n'est focus, on met le curseur sur celui-ci
+                }
+
+                if (ErreursEntrees != "") //Si erreurs, on affiche une messageBox
+                {
+                    MessageBox.Show(ErreursEntrees, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    try
+                    {
+                        decimal plafondMecenat = decimal.Parse(txtSommeMax.Text);
+                        Pays newPays = new Pays((int)cbxPays.SelectedValue, cbxPays.SelectedText);
+                        Annee newAnnee = new Annee((int)cbxAnnee.SelectedValue);
+                        Pays ancienPays = new Pays(idPays);
+                        Annee ancienneAnnee = new Annee(idAnnee);
+
+                        int nb = AvoirPourLimiteDeMecenatManager.GetInstance().ModifLimiteMecenat(plafondMecenat, newPays, newAnnee, ancienPays, ancienneAnnee);
+                        if (nb == -1)
+                        {
+                            MessageBox.Show("Modification réussie", "Réussite", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("La modification n'a pas fonctionnée", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                    catch (Exception uneException)
+                    {
+                        MessageBox.Show("Problème au niveau de la modification : " + uneException.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+                majCbx();
+            }
+        }
+        private void btnAnnuler_Click(object sender, EventArgs e)
+        {
+            txtSommeMax.Text = "";
+
+            cbxAnnee.SelectedIndex = -1;
+            cbxPays.SelectedIndex = -1;
         }
 
     }
